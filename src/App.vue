d6cd33c72da351784c64555c2b28af33b1a3c601
TammiChia <e0555851@u.nus.edu>
2022-10-11 20:53:47 +0800

    Formatting of Navigation Bar

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <template>
   <div> 
     <nav id="nav">
-      <router-link to="/">Home</router-link>
-      <router-link to="/search">Search</router-link>
-      <router-link to="/booking">Booking</router-link>
-      <router-link to="/profile">Profile</router-link>
-      <router-link to="/login">Login</router-link>
-      <router-link to="/signup">SignUp</router-link>
+      <ul>
+        <li><router-link to="/signup">SignUp</router-link></li>
+        <li><router-link to="/login">Login</router-link></li>
+        <li><router-link to="/profile">Profile</router-link></li>
+        <li><router-link to="/booking">Booking</router-link></li>
+        <li><router-link to="/search">Search</router-link></li>
+        <li><router-link to="/">Home</router-link></li>
+      </ul>
     </nav>
     <router-view />
   </div>
@@ -18,15 +20,37 @@ export default {
 }
 </script>
 
-<style>
-#app {
-  colour: black; 
-  font-family: 'Courier New', Courier, monospace;  
+<style scoped>
+ul {
+  font-family: Arial, Helvetica, sans-serif;
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+  background-color: white;
+  position: fixed;
+  top: 0;
+  width: 100%;
+  height: 10%;
+  border-bottom: 2px solid grey;
+  line-height: 50px;
 }
 
-#nav {
-  font-family: Arial, Helvetica, sans-serif;
+li {
+  float: right;
+}
+
+li a {
+  display: block;
+  color: black;
   text-align: center;
-  padding: 20px;
+  padding: 14px 16px;
+  text-decoration: none;
+  font-size: 120%;
+  margin: 7px;
+}
+
+li a:hover {
+  color: orange;
 }
 </style>
