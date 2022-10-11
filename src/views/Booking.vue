20a087be4f55cd8bc6725f4e259695397a492403
TammiChia <e0555851@u.nus.edu>
2022-10-11 21:27:33 +0800

    Update Booking.vue

--- src/views/Booking.vue
+++ src/views/Booking.vue
@@ -1,22 +1,26 @@
 <template>
     <h1> Booking Page </h1>
-    <div class="upcoming">
-        <h1 style="text-align: left">Upcoming bookings</h1>
+<div class="booking">
+    <div id="upcoming">
+        <h1 style="text-align: left">Upcoming Bookings</h1>
     </div>
-    <div class="previous">
-        <h1 style="text-align: left">Previous bookings</h1>
+    <div id="previous">
+        <h1 style="text-align: left">Previous Bookings</h1>
     </div>
+</div>
 </template>
 
 <style>
-.upcoming {
+.booking {
+    font-family: Arial, Helvetica, sans-serif;
+}
+#upcoming {
   margin-left: 30px;
   height: 300px;
   width: 100%;
   
 }
-
-.previous { 
+#previous { 
   margin-left: 30px;
   height: 300px;
   width: 100%;
