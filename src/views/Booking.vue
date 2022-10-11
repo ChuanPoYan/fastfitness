75c08c954e5fe78666d0dff58d3da97b8fec619c
TammiChia <e0555851@u.nus.edu>
2022-10-11 21:23:53 +0800

    Create Booking page

--- src/views/Booking.vue
+++ src/views/Booking.vue
@@ -1,3 +1,25 @@
 <template>
     <h1> Booking Page </h1>
+    <div class="upcoming">
+        <h1 style="text-align: left">Upcoming bookings</h1>
+    </div>
+    <div class="previous">
+        <h1 style="text-align: left">Previous bookings</h1>
+    </div>
 </template>
+
+<style>
+.upcoming {
+  margin-left: 30px;
+  height: 300px;
+  width: 100%;
+  
+}
+
+.previous { 
+  margin-left: 30px;
+  height: 300px;
+  width: 100%;
+}
+</style>
+
