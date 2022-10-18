1f257942bbaba90d271104ecaddcf39fb2d28574
joeltohyiliang <joeltohyiliang99@gmail.com>
2022-10-18 17:22:39 +0800

    added profileaccountpage
    added profileaccount page

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router/index.js'
 import { initializeApp } from 'firebase/app'
 
+
 // Import the functions you need from the SDKs you need
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
