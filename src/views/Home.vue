e4361b7cfb912482d53a947fa6cafa713962fcc7
TammiChia <e0555851@u.nus.edu>
2022-10-18 20:27:42 +0800

    home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,3 +1,4 @@
 <template>
     <h1> Home Page </h1>
+    <img src="../assets/home.png" style="float: height: 80%; left; width: auto; height: auto; margin: 10px 10px 10px 10px" alt="poyan">
 </template>
