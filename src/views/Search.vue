a2be21e331f11f7fe1b74f5aea134d24c6910409
Po Yan <70246850+ChuanPoYan@users.noreply.github.com>
2022-10-18 17:17:47 +0800

    Update Search Page

--- src/views/Search.vue
+++ src/views/Search.vue
@@ -1,3 +1,60 @@
 <template>
-    <h1> Search Page </h1>
-</template>
+    <div>
+      <div style="width: 67%; float: left">
+        <div>
+          <input type="text" placeholder="Search.." class="topnav"/>
+          <select v-model="selected" class="topnav">
+            <option disabled value="">Category</option>
+            <option>Exercise</option>
+            <option>Karate</option>
+            <option>Yoga</option>
+            <option>Meditation</option>
+          </select>
+        </div>
+        <div class="child inline-block-child">
+          <BookingListing />
+          <BookingListing />
+        </div>
+        <div class="child inline-block-child">
+          <BookingListing />
+          <BookingListing />
+        </div>
+      </div>
+      <div style="margin-left: 67%; ">
+        <h3 class="title">Studio Location</h3>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  import BookingListing from '@/components/BookingListing.vue'
+  
+  export default {
+    name: "App",
+    components: {
+      BookingListing,
+    },
+  };
+  </script>
+  
+  <style>
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+  
+  .inline-block-child {
+    display: inline-block;
+  }
+  
+  .topnav {
+    font-size: 20px;
+    width: 400px;
+    margin: 20px;
+  }
+  </style>
+  
