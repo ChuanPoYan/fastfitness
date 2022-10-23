4b44ccf846b8164e602907924875cdecc8b04f7f
joeltohyiliang <joeltohyiliang99@gmail.com>
2022-10-24 01:58:33 +0800

    Overrided the old Profile Membership
    Filled up details for profile Membership

--- src/views/ProfileMembership.vue
+++ src/views/ProfileMembership.vue
@@ -1,55 +1,87 @@
 <template>
-  <br /><br /><br /><br /><br /><br /><br />
-  <h1>Our Packages</h1>
-  <div class="container">
-    <div class="col">
-      <h2>STARTER PACKAGE</h2>
-      <hr />
-      <p>$50/MONTH</p>
-      <hr />
-      <p>About 4-5 classes</p>
-      <hr />
-      <p>Free Lanyard</p>
-      <hr />
-      <p>Bronze Member</p>
-      <hr />
-      <p>Access to ALL classes</p>
-      <div v-if="'Starter' == this.membership">Selected</div>
+  <body>
+  <br><br><br><br><br>
+  <div class="main">
+    <div class="sidebar">
+      <h2>sidebar</h2>
+    </div>
+    <div class ="Credits">
+      <!-- <h1>Credits</h1> -->
+      <br><br><br><br>
+      <div class = "card2">
+        <h2>Starter Package</h2>
+        <hr/>
+        <br>
+        <h1>50/month</h1>
+        <br>
+        <hr/>
+        <h3>4-5 Classes</h3>
+        <hr/>
+        <p>Free Lanyard</p>
+        <hr/>
+        <p>bronze Member</p>
+        <hr/>
+        <p>Access to all classes</p>
+        <div v-if="'Starter' == this.membership">Selected</div>
       <button v-else @click="select('Starter')">Select</button>
+      </div> 
     </div>
-    <div class="col">
-      <h2>INTERMEDIATE PACKAGE</h2>
-      <hr />
-      <p>$90/MONTH</p>
-      <hr />
-      <p>About 6-9 classes</p>
-      <hr />
-      <p>Free T-shirt</p>
-      <hr />
-      <p>Silver Member</p>
-      <hr />
-      <p>Access to ALL classes</p>
-      <div v-if="'Intermediate' == this.membership">Selected</div>
-      <button v-else @click="select('Intermediate')">Select</button>
+
+
+    
+      <div class ="Credits">
+      <h1>Our Packages</h1>
+      <div class = "card2">
+        <h2>Intermediate Package</h2>
+        <hr/>
+        <br>
+        <h1>90/month</h1>
+        <br>
+        <hr/>
+        <h3>6-9 Classes</h3>
+        <hr/>
+        <p>Free Shirt</p>
+        <hr/>
+        <p>silver Member</p>
+        <hr/>
+        <p>Access to all classes</p>
+        <div v-if="'Intermediate' == this.membership">Selected</div>
+  <button v-else @click="select('Intermediate')">Select</button>
+      </div> 
     </div>
-    <div class="col">
-      <h2>ADVANCE PACKAGE</h2>
-      <hr />
-      <p>$120/MONTH</p>
-      <hr />
-      <p>About 10-15 classes</p>
-      <hr />
-      <p>Free Resistance Bands</p>
-      <hr />
-      <p>Gold Member</p>
-      <hr />
-      <p>Access to ALL classes</p>
-      <div v-if="'Advance' == this.membership">Selected</div>
+
+
+
+    <div class ="intermediate">
+      <br><br><br><br>
+      <div class = "card2">
+        <h2>Advanced Package</h2>
+        <hr/>
+        <br>
+        <h1>120/month</h1>
+        <br>
+        <hr/>
+        <h3>10-15 Classes</h3>
+        <hr/>
+        <p>Free Resistance Beads</p>
+        <hr/>
+        <p>Gold Member</p>
+        <hr/>
+        <p>Access to all classes</p>
+        <div v-if="'Advance' == this.membership">Selected</div>
       <button v-else @click="select('Advance')">Select</button>
+      </div> 
     </div>
   </div>
-</template>
-
+  </body>
+<div class="sidenav">
+    <router-link to="/profile">Profile</router-link>
+    <router-link to="/profileAccount">Account</router-link>
+    <router-link to="/profileHistory">Profile History</router-link>
+    <router-link to="/profileMembership">Membership</router-link>
+    <router-view/>  
+  </div>
+  </template>
 <script>
 import firebaseApp from "../main.js";
 import { getFirestore } from "firebase/firestore";
@@ -93,20 +125,59 @@ export default {
 </script>
 
 <style>
-.container {
-  width: 1200px;
-  height: 400px;
-  background-color: green;
-  padding-top: 40px;
-  text-align: center;
-}
 
-.col {
-  float: left;
-  width: 360px;
-  height: 360px;
-  background-color: white;
-  border: solid black;
-  margin-left: 20px;
-}
+.main{
+    display: grid;
+    grid-template-columns: 350px 1fr 1fr 1fr;
+  }
+  
+  /* header{
+    height:20%;
+  }, */
+  .sidenav {
+    margin-top: 95px !important;
+    height: 100%; 
+    width: 300px; /* Set the width of the sidebar */
+    position: fixed; /* Fixed Sidebar (stay in place on scroll) */
+    z-index: 1; /* Stay on top */
+    top: 0; /* Stay at the top */
+    left: 0;
+    background-color: rgba(241, 241, 241, 1); /* Black */
+    overflow-x: hidden; /* Disable horizontal scroll */
+    padding-top: 100px;
+  }
+  .sidenav a {
+    padding: 6px 8px 6px 16px;
+    text-decoration: none;
+    font-size: 25px;
+    color: black;
+    display: block;
+  }
+  .card2{
+    background-color:rgba(241, 241, 241,1);
+    text-align: center;
+    box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+    border-radius: 5px; /* 5px rounded corners */
+    width: 400px;
+    height: 500px;
+    padding-top: 20px;
+  }
+  button{
+      border: none;
+      background-color: orange;
+      width: 50%;
+      margin: 10px auto;
+      padding: 10px 30px;
+      color: white;
+      text-transform: uppercase;
+  }
+  hr{
+      display: block;
+      border: none;
+      height: 3px;
+      background-color: grey;
+  }
+  h1{
+    text-align: center;
+  }
 </style>
