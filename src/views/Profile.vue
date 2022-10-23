3a8eb5ab56f6e2ccccf02f7425b7a46b699c9063
Po Yan <70246850+ChuanPoYan@users.noreply.github.com>
2022-10-23 21:29:06 +0800

    Added Membership backend

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,108 +1,107 @@
 <template>
-    <br>
-<div class="AddEmployeeForm">
-    <h1> Personal information </h1>
-       <label for="name"> Name: </label>
-    <input type ="text" id = "name"  placeholder="Name" v-model="name" /> 
-    <br><br>
+  <br />
+  <div class="AddEmployeeForm">
+    <h1>Personal information</h1>
+    <label for="name"> Name: </label>
+    <input type="text" id="name" placeholder="Name" v-model="name" />
+    <br /><br />
 
     <label for="email"> Email: </label>
-    <input type="text" id = "email"  placeholder="Email" v-model="email" /> 
-    <br><br>
+    <input type="text" id="email" placeholder="Email" v-model="email" />
+    <br /><br />
 
     <label for="Phone"> Phone Number </label>
-    <input type="text" id = "number"  placeholder="number" v-model="number" /> 
-    <br><br>
+    <input type="text" id="number" placeholder="number" v-model="number" />
+    <br /><br />
 
-    <label for="Address"> Address: </label> 
-    <input type ="text" id = "address"  placeholder="Address" v-model="address" /> 
-    <br><br>
+    <label for="Address"> Address: </label>
+    <input type="text" id="address" placeholder="Address" v-model="address" />
+    <br /><br />
     <v-layout row>
-      <v-flex  md6 offset-sm3 >
-       <div>
-         <button @click="saveChanges"> Save Changes </button>
-       </div>
-       </v-flex>
+      <v-flex md6 offset-sm3>
+        <div>
+          <button @click="saveChanges">Save Changes</button>
+        </div>
+      </v-flex>
     </v-layout>
-</div>
+  </div>
 
-<div class="sidenav">
-    
-    
+  <div class="sidenav">
     <router-link to="/profile">Profile</router-link>
-    <router-link to="/profileAccount">Account</router-link>
-    <router-link to="/profileHistory">Profile History</router-link>
-    <router-link to="/profileMembership">Membership</router-link>
-    
-    <router-view/>
-  <!-- <a class ="active" href="#personalinfo">Personal information</a>
+    <router-link to="/profile/account">Account</router-link>
+    <router-link to="/profile/history">Profile History</router-link>
+    <router-link to="/profile/membership">Membership</router-link>
+
+    <router-view />
+    <!-- <a class ="active" href="#personalinfo">Personal information</a>
   <a href="#account">Account</a>
   <a href="#membership">Membership</a>
   <a href="#paymenthistory">Payment History</a> -->
-  
-</div>
-<br><br>
+  </div>
+  <br /><br />
 </template>
 
 <script>
-import firebaseApp from '../main.js';
-import { getFirestore } from "firebase/firestore"
-import { doc, updateDoc } from "firebase/firestore"
+import firebaseApp from "../main.js";
+import { getFirestore } from "firebase/firestore";
+import { doc, updateDoc } from "firebase/firestore";
+
 const db = getFirestore(firebaseApp);
 
 export default {
-    name: "SaveProfileChanges",
-    data() {
-        return {
-            name: "",
-            email: "",
-            number: "",
-            address: ""
-        }
+  name: "SaveProfileChanges",
+  data() {
+    return {
+      name: "",
+      email: "",
+      number: "",
+      address: "",
+    };
+  },
+  methods: {
+    saveChanges() {
+      const userRef = doc(db, "users", this.email);
+      updateDoc(userRef, {
+        First_Name: this.name,
+        Phone_Number: this.number,
+        Email: this.email,
+        Address: this.address,
+      }).catch((error) => {
+        console.error("Error Saving Information", error);
+      });
     },
-    methods: {
-        saveChanges() {
-            const userRef = doc(db, "users", this.email);
-            updateDoc(userRef, {First_Name: this.name, Phone_Number: this.number, 
-                Email: this.email, Address: this.address
-            })
-            .catch((error) => {
-                console.error("Error Saving Information", error)
-            })
-        }
-    }
+  },
 };
 </script>
 
 <style>
-    .header{
-        height: 50px;
-        background-color: grey;
-        text-align: center;
-        font-size: 17px;
-    }
-    #desc{
-        height: 50px;
-        background-color: none;
-        text-align: center;
-        font-size: 16px;
-    }
-    .AddEmployeeForm{
-        text-align: left;
-        font-size: 20px;
-        width: 382px;  
-        overflow: hidden;  
-        margin: auto;   
-        padding: 100px;  
-        background: #ffffff;  
-        border-radius: 15px ;
-    }
-    
+.header {
+  height: 50px;
+  background-color: grey;
+  text-align: center;
+  font-size: 17px;
+}
+#desc {
+  height: 50px;
+  background-color: none;
+  text-align: center;
+  font-size: 16px;
+}
+.AddEmployeeForm {
+  text-align: left;
+  font-size: 20px;
+  width: 382px;
+  overflow: hidden;
+  margin: auto;
+  padding: 100px;
+  background: #ffffff;
+  border-radius: 15px;
+}
 
-    /* The sidebar menu */
+/* The sidebar menu */
 .sidenav {
   margin-top: 95px !important;
-  height: 100%; 
+  height: 100%;
   width: 300px; /* Set the width of the sidebar */
   position: fixed; /* Fixed Sidebar (stay in place on scroll) */
   z-index: 1; /* Stay on top */
@@ -134,24 +133,26 @@ export default {
 .main {
   margin-left: 200px; /* Same as the width of the sidebar */
   padding: 0px 10px;
-  font-family: Arial, Helvetica, sans-serif; 
+  font-family: Arial, Helvetica, sans-serif;
 }
-.input-contain{
-    position: relative;
+.input-contain {
+  position: relative;
 }
-input{
-    height: 5rem;
-    width: 20rem;
-    border: 2px solid black;
-    border-radius: 1rem;
+input {
+  height: 5rem;
+  width: 20rem;
+  border: 2px solid black;
+  border-radius: 1rem;
 }
-input:focus{
-    outline: none;
-    border-color: orange;
+input:focus {
+  outline: none;
+  border-color: orange;
 }
 
-input, .placeholder{
-    font-size: 1.4rem;
-    padding: 0 1.2rem;
+input,
+.placeholder {
+  font-size: 1.4rem;
+  padding: 0 1.2rem;
 }
-</style>>
+</style>
+>
--- src/views/ProfileMembership.vue
+++ src/views/ProfileMembership.vue
@@ -1,13 +1,108 @@
 <template>
-    <h1>Membership Page</h1>
+  <br /><br /><br /><br /><br /><br /><br />
+  <h1>Our Packages</h1>
+  <div class="container">
+    <div class="col">
+      <h2>STARTER PACKAGE</h2>
+      <hr />
+      <p>$50/MONTH</p>
+      <hr />
+      <p>About 4-5 classes</p>
+      <hr />
+      <p>Free Lanyard</p>
+      <hr />
+      <p>Bronze Member</p>
+      <hr />
+      <p>Access to ALL classes</p>
+      <div v-if="'Starter' == this.membership">Selected</div>
+      <button v-else @click="select('Starter')">Select</button>
+    </div>
+    <div class="col">
+      <h2>INTERMEDIATE PACKAGE</h2>
+      <hr />
+      <p>$90/MONTH</p>
+      <hr />
+      <p>About 6-9 classes</p>
+      <hr />
+      <p>Free T-shirt</p>
+      <hr />
+      <p>Silver Member</p>
+      <hr />
+      <p>Access to ALL classes</p>
+      <div v-if="'Intermediate' == this.membership">Selected</div>
+      <button v-else @click="select('Intermediate')">Select</button>
+    </div>
+    <div class="col">
+      <h2>ADVANCE PACKAGE</h2>
+      <hr />
+      <p>$120/MONTH</p>
+      <hr />
+      <p>About 10-15 classes</p>
+      <hr />
+      <p>Free Resistance Bands</p>
+      <hr />
+      <p>Gold Member</p>
+      <hr />
+      <p>Access to ALL classes</p>
+      <div v-if="'Advance' == this.membership">Selected</div>
+      <button v-else @click="select('Advance')">Select</button>
+    </div>
+  </div>
 </template>
 
 <script>
+import firebaseApp from "../main.js";
+import { getFirestore } from "firebase/firestore";
+import { doc, getDoc, updateDoc } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+
+const db = getFirestore(firebaseApp);
+
 export default {
-    name:"ProfileMembership",
-}
+  name: "ProfileMembership", 
+  data(){
+   return {membership : null}
+  }, 
+  created: async function(){
+    const auth = getAuth(firebaseApp);
+      this.email = auth.currentUser.email;
+      const docRef = doc(db, "users", this.email);
+      getDoc(docRef).then((result) => {
+        if (result.exists()) {
+          this.membership = result.data()["Membership"]; 
+        }
+      });
+  }, 
+  methods: {
+    select(membership) {
+      const auth = getAuth(firebaseApp);
+      this.email = auth.currentUser.email;
+      const userRef = doc(db, "users", this.email);
+      updateDoc(userRef, {
+        Membership: membership, 
+      }).catch((error) => {
+        console.error("Error Saving Information", error);
+      });
+    },
+  },
+};
 </script>
 
 <style>
+.container {
+  width: 1200px;
+  height: 400px;
+  background-color: green;
+  padding-top: 40px;
+  text-align: center;
+}
 
+.col {
+  float: left;
+  width: 360px;
+  height: 360px;
+  background-color: white;
+  border: solid black;
+  margin-left: 20px;
+}
 </style>
