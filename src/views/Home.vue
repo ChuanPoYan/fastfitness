02a2ba3a465cf694d1d4de783289ee06bbb6b708
TammiChia <e0555851@u.nus.edu>
2022-10-24 18:54:15 +0800

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,28 @@
 <template>
-<div class="child" id="pic">
-    <img src="../assets/homepage.png" alt="homepage">
-</div>
-<div class="child" id="card">
+<div class="container">
+
+  <div class="left">
+      <img src="../assets/homepage.png" alt="homepage">
+  </div>
+
+  <div class="right" id="card">
     <img src="../assets/promo2.png" alt="Promotion1" style="width:100%">
-    <div class="container">
-    <h4><b>Collaboration with Kydra Activewear!</b></h4>
-    <p>Up to 75% discounts if you spend a minimum of $50!</p>
-    <h5><b>Promotion up till 31st December 2022</b></h5>
+    <div class="text">
+      <h4><b>Collaboration with Kydra Activewear!</b></h4>
+      <p>Up to 75% discounts if you spend a minimum of $50!</p>
+      <h5><b>Promotion up till 31st December 2022</b></h5>
     </div>
+  </div>
+
+  <div class="right" id="card">
+    <img src="../assets/promo3.png" alt="Promotion1" style="width:100%">
+    <div class="text">
+      <h4><b>Collaboration with Barry's Bootcamp!</b></h4>
+      <p>Get a complimentary class to Barry's Bootcamp if you purchase the Advanced Package!</p>
+      <h5><b>Promotion up till 31st December 2022</b></h5>
+    </div>
+  </div>
+
 </div>
 </template>
 
@@ -19,14 +33,25 @@ export default {
 </script>
 
 <style scoped>
-#pic {
-    padding-top: 20px;
-    width: 800px;
-    height: 600px;  
+.container {  
+  padding-top: 30px;
+  display: grid;
+  grid-template-columns: 1.3fr 0.7fr;
+  grid-template-rows: 2.4fr;
+  gap: 0px 41px;
+  grid-auto-flow: row;
+  grid-template-areas:
+    ". .";
+}
+
+.left {
+    width: 1000px;
+    height: 800px;  
 }
 /* card */
 #card {
   /* Add shadows to create the "card" effect */
+  padding-top: 70px;
   box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
   transition: 0.3s;
   border-radius: 5px; /* 5px rounded corners */
@@ -34,7 +59,6 @@ export default {
   height: 500px;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   float: left; 
-  padding-top: 20px;
 }
 
 /* On mouse-over, add a deeper shadow */
@@ -46,5 +70,9 @@ export default {
 .container {
   padding: 2px 16px;
 }
-        
+
+.text {
+  text-align: center;
+}
+       
 </style>
