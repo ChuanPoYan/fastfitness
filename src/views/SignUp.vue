33a866c8346104d01f6e676ce7c6327d29a79777
TammiChia <e0555851@u.nus.edu>
2022-10-25 16:24:01 +0800

    Formatting all the buttons

--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -6,7 +6,7 @@
       <br /><br />
       <input type="password" placeholder="Password..." v-model="password" />
       <br /><br />
-      <button>Sign Up</button>
+      <button class="button">Sign Up</button>
       <p>Signed Up Already? Login</p>
     </form>
   </div>
@@ -72,5 +72,23 @@ export default {
 #signup {
   text-align: center;
   padding-top: 80px;
+}
+
+.button{ 
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    padding: 1rem 3rem;
+    text-align: center;
+    font-size: 16px;
+    text-transform: uppercase;
+    cursor: pointer;
+    background: rgba(255,106,40);
+    border-radius: 9px;
+    border: none;
+    color: #fff;
+    font-weight: bold;
+    letter-spacing: 1px;
+}
+.button:hover {
+  box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
 }
 </style>
