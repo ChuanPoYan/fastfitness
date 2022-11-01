262e748e08459580870f84684f2672e1c58bcc09
TammiChia <e0555851@u.nus.edu>
2022-11-01 22:41:39 +0800

    Update BookingInformation.vue

--- src/views/BookingInformation.vue
+++ src/views/BookingInformation.vue
@@ -4,10 +4,9 @@
   <div class="sub">
     <img src="../assets/spin.png" alt="Spin" style="width: 100%" />
   </div>
-
-  <div class="sub" id="information">
-    <h1> Booking Information </h1>
-    <div>
+  <div class="sub">
+    <div class="information">
+      <h1> Booking Information </h1>
       <h3> Category: Spin </h3>
       <h3> Studio: Ride the Current!  </h3>
       <h3> Instructor: Mandalyn Tan  </h3>
@@ -16,21 +15,16 @@
       <h3> Description: Ride the Current! puts the fun factor into getting your sweat on. You decide how far to push yourself, their instructors are there to guide and encourage you the whole ride.
         It is the ultimate party on a bike experience for anyone looking for that full body workout </h3>
     </div>
-
     <div class="booking">
       <h3 > Pick an available time! </h3>
     </div>
-
+    
   </div>
-
 </div>
-  
 </template>
 
 <script>
-export default {
 
-}
 </script>
 
 <style>
@@ -43,9 +37,6 @@ export default {
 .sub {
   width: 50%; 
   padding: 60px;
-}
-
-#information {
   font-family: Avenir, Helvetica, Arial, sans-serif;
 }
 
@@ -54,7 +45,8 @@ export default {
   text-align: center;
   color: white; 
   border-radius: 30px;
-  height: 200px; 
+  height: 500px; 
+
 }
 
 
