bb30c3965e4909da2ad47fa854d4ed18345447a8
TammiChia <e0555851@u.nus.edu>
2022-11-04 15:30:44 +0800

    Redo Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,96 +1,71 @@
 <template>
-  <div class="container">
-    <div class="left">
-      <img src="../assets/homepage.png" alt="homepage" />
-    </div>
-    <div class="innerright">
-      <div class="card">
-        <img src="../assets/promo2.png" alt="Promotion1" style="width: 100%" />
-        <div class="text">
-          <h4><b>Collaboration with Kydra Activewear!</b></h4>
-          <p>Up to 75% discounts if you spend a minimum of $50!</p>
-          <h5><b>Promotion up till 31st December 2022</b></h5>
-        </div>
-      </div>
+<div class="main">
 
-      <div class="card">
-        <img src="../assets/promo3.png" alt="Promotion1" style="width: 100%" />
-        <div class="text">
-          <h4><b>Collaboration with Barry's Bootcamp!</b></h4>
-          <p>
-            Get a complimentary class to Barry's Bootcamp if you purchase the
-            Advanced Package!
-          </p>
-          <h5><b>Promotion up till 31st December 2022</b></h5>
-        </div>
-      </div>
-    </div>
+  <div class="sub">
+    <img src="../assets/homepage.png" alt="homepage" />
   </div>
+
+  <div class="sub">
+  <!-- CARD 1 -->
+  <div class="information1">
+        <img src="../assets/promo4.png" alt="Promotion1" style="object-fit: cover; width: 80%" />
+        <h4><b>Collaboration with Kydra Activewear!</b></h4>
+        <p>Up to 75% discounts if you spend a minimum of $50!</p>
+        <h5><b>Promotion up till 31st December 2022</b></h5>
+  </div>
+  <br>
+  <!-- CARD 2 -->
+  <div class="information2">
+        <img src="../assets/promo3.png" alt="Promotion1" style="width: 80%" />
+        <h4><b>Collaboration with Barry's Bootcamp!</b></h4>
+        <p> FREE Barry's Bootcamp Class for GOLD members! </p>
+        <h5><b>Promotion up till 31st December 2022</b></h5>
+  </div>
+  </div>
+</div>
 </template>
 
 <script>
 export default {
-  name: "Home",
-};
+
+}
 </script>
 
-<style scoped>
-.container {
-  padding-top: 30px;
-  display: grid;
-  /* grid-template-columns: 1.3fr 0.7fr;
-  grid-template-rows: 2.4fr;
-  gap: 0px 41px;
-  grid-auto-flow: row;
-  grid-template-areas:
-    ". ."; */
+<style>
+.main{
+  padding-top: 50px;
+  width: 100%; 
+  display: flex;
+}
 
-  grid-template-columns: repeat(3, 1fr);
-  grid-gap: 20px;
-  /* background-color: grey; */
-  /* padding: 2px 16px; */
-  /* grid-auto-flow: row; */
-}
-.container img {
-  width: 87%;
-  height: auto;
-  background-position: center;
-}
-.left {
-  width: 1200px;
-  height: 800px;
-  /* background-color: orange; */
-  /* float:left */
-  /* align-content: center; */
-  overflow: auto;
-}
-.innerright {
-  display: grid;
-  grid-template-rows: repeat(2, 1fr);
-  grid-gap: 10px;
-  /* overflow:auto; */
-  /* padding-top: 100px; */
-}
-/* card */
-.card {
-  /* Add shadows to create the "card" effect */
-  /* padding-top: 70px; */
-  background-color: white;
-  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
-  /* transition: 0.3s; */
-  border-radius: 5px; /* 5px rounded corners */
-  width: 400px;
-  height: 380px;
+.sub {
+  width: 90%; 
+  padding: 30px;
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  float: left;
 }
 
-/* On mouse-over, add a deeper shadow */
-.card:hover {
-  box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2);
-}
-
-.text {
+.information1 {
   text-align: center;
+  background-colour: white;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+  border-radius: 5px; 
+  
+  line-height: 0.2em;
+  width: 500px;
+  height: 300px;
 }
+
+.information2 {
+  text-align: center;
+  background-colour: white;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+  border-radius: 5px; 
+
+  line-height: 0.2em;
+  width: 500px;
+  height: 300px;
+
+}
+
 </style>
+
