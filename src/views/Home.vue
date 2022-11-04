5a085759c4b4da2e89961ad1cb68a63d09e73f61
TammiChia <e0555851@u.nus.edu>
2022-11-04 15:47:46 +0800

    Fix images on Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,14 @@
 <template>
 <div class="main">
-
+  <!-- LEFT COMPONENT -->
   <div class="sub">
-    <img src="../assets/homepage.png" alt="homepage" />
+    <img src="../assets/homepage.png" alt="homepage" style="width: 840px" />
   </div>
-
+  <!-- RIGHT COMPONENT -->
   <div class="sub">
   <!-- CARD 1 -->
   <div class="information1">
-        <img src="../assets/promo4.png" alt="Promotion1" style="object-fit: contain; width: 80%" />
+        <img src="../assets/promo4.png" alt="Promotion1" style="width: 480px" />
         <h4><b>Collaboration with Kydra Activewear!</b></h4>
         <p>Up to 75% discounts if you spend a minimum of $50!</p>
         <h5><b>Promotion up till 31st December 2022</b></h5>
@@ -16,7 +16,7 @@
   <br>
   <!-- CARD 2 -->
   <div class="information2">
-        <img src="../assets/promo3.png" alt="Promotion1" style="width: 80%" />
+        <img src="../assets/promo3.png" alt="Promotion1" style="width: 480px" />
         <h4><b>Collaboration with Barry's Bootcamp!</b></h4>
         <p> FREE Barry's Bootcamp Class for GOLD members! </p>
         <h5><b>Promotion up till 31st December 2022</b></h5>
@@ -50,8 +50,8 @@ export default {
   box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
   border-radius: 5px; 
   line-height: 0.5em;
-  width: 500px;
-  height: 320px;
+  width: 480px;
+  height: 330px;
 }
 
 .information2 {
@@ -60,8 +60,8 @@ export default {
   box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
   border-radius: 5px; 
   line-height: 0.5em;
-  width: 500px;
-  height: 320px;
+  width: 480px;
+  height: 330px;
 }
 
 </style>
