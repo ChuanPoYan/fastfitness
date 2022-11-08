f862cf021b37cb775cbcee20cf98874420ffe222
Po Yan <70246850+ChuanPoYan@users.noreply.github.com>
2022-11-08 15:19:47 +0800

    Added BookingInformation backend

--- src/views/BookingInformation.vue
+++ src/views/BookingInformation.vue
@@ -1,66 +1,71 @@
 <template>
-<div>
-  <SavedModal v-show="showModal" @close-modal="showModal = false" />
-</div>
-<div class="main">
-
-  <div class="sub">
-
-    <img id="photo" :src="require(`@/${this.classPhoto}`)" :alt='this.className' style="width: 100%" />
+  <div>
+    <SavedModal v-show="showModal" @close-modal="showModal = false" />
   </div>
+  <div class="main">
+    <div class="sub">
+      <img
+        id="photo"
+        :src="require(`@/${this.classPhoto}`)"
+        :alt="this.className"
+        style="width: 100%"
+      />
+    </div>
 
-  <div class="sub">
-    <div class="information">
-      <div class="information1">
-        <p id="classinformation"> CLASS INFORMATION </p>
-        <h1 id="studio"> {{this.classVenue}}  </h1>
-        <h4 id="credits"> {{this.classPrice}} CREDITS </h4>
-      <hr>
+    <div class="sub">
+      <div class="information">
+        <div class="information1">
+          <p id="classinformation">CLASS INFORMATION</p>
+          <h1 id="studio">{{ this.classVenue }}</h1>
+          <h4 id="credits">{{ this.classPrice }} CREDITS</h4>
+          <hr />
+        </div>
+        <div class="information2">
+          <p><b>CATEGORY:</b> {{ this.className }}</p>
+          <p><b>INSTRUCTOR:</b> {{ this.classInstructor }}</p>
+          <p><b>DURATION:</b> 50 MINS</p>
+          <p>{{ this.classDescription }}</p>
+        </div>
       </div>
-      <div class="information2">
-        <p><b>CATEGORY:</b> {{this.className}}  </p>
-        <p><b>INSTRUCTOR:</b> {{this.classInstructor}}  </p>
-        <p><b>DURATION:</b> 50 MINS </p>
-        <p>{{this.classDescription}} </p>
+      <br />
+      <div class="booking">
+        <h3>Select an available date!</h3>
+        <!-- calendar datepicker-->
+        <div class="calendar">
+          <label for="datepicker">Date</label>
+          <datepicker v-model="picked" id="datepicker" />
+          <br />
+        </div>
+        <div class="bookingbutton">
+          <button id="button" @click="booking()">BOOK</button>
+          <button id="button">CANCEL</button>
+        </div>
       </div>
     </div>
-    <br>
-    <div class="booking">
-      <h3> Select an available date! </h3>
-      <!-- calendar datepicker-->
-      <div class="calendar">
-			<label for="datepicker">Date</label>
-			<datepicker v-model="picked" id="datepicker"/>
-      <br>
-      </div>
-      <div class="bookingbutton">
-        <button id="button" @click="booking()"> BOOK </button> 
-        <button id="button"> CANCEL </button>
-      </div>
-    </div>
   </div>
-</div>
 </template>
 
 <script>
 //Import Datepicker for calendar
-import Datepicker from 'vue3-datepicker'
-import SavedModal from '../components/SavedModal.vue'
+import Datepicker from "vue3-datepicker";
+import SavedModal from "../components/SavedModal.vue";
 
 //Firebase imports
 import firebaseApp from "../main.js";
 import { getFirestore } from "firebase/firestore";
 import { doc, getDoc } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
 
 const db = getFirestore(firebaseApp);
 
 export default {
-	components: {
-		Datepicker,
+  components: {
+    Datepicker,
     SavedModal,
-	},
+  },
   data() {
     return {
+      classID: null,
       className: "Description",
       classCapacity: null,
       classCategory: null,
@@ -76,25 +81,33 @@ export default {
     booking() {
       //Need method for this cause will extend later on
       this.showModal = true;
-    }
+    },
   },
   //Get details based on classID
   created: async function () {
-    const docRefClass = doc(db, "Class", "Rugby1");
-    getDoc(docRefClass).then((result) => {
-      if (result.exists()) {
-        this.className = result.data()["Name"];
-        this.classCapacity = result.data()["Capacity"];
-        this.classCategory = result.data()["Category"];
-        this.classInstructor = result.data()["Instructor"];
-        this.classPrice = result.data()["Price"];
-        this.classVenue = result.data()["Venue"];
-        this.classPhoto = result.data()["Photo"];
-        this.classDescription = result.data()["Description"];
+    const auth = await getAuth(firebaseApp);
+    var email = auth.currentUser.email;
+    const usersDocRef = doc(db, "users", email);
+    getDoc(usersDocRef).then((userDoc) => {
+      if (userDoc.exists()) {
+        this.classID = userDoc.data()["Viewing"];
+        const docRefClass = doc(db, "Class", this.classID);
+        getDoc(docRefClass).then((result) => {
+          if (result.exists()) {
+            this.className = result.data()["Name"];
+            this.classCapacity = result.data()["Capacity"];
+            this.classCategory = result.data()["Category"];
+            this.classInstructor = result.data()["Instructor"];
+            this.classPrice = result.data()["Price"];
+            this.classVenue = result.data()["Venue"];
+            this.classPhoto = result.data()["Photo"];
+            this.classDescription = result.data()["Description"];
+          }
+        });
       }
     });
-  }
-}
+  },
+};
 </script>
 
 <style>
@@ -104,16 +117,16 @@ export default {
 
 .information2 {
   font-weight: 500;
-  letter-spacing: 1px; 
+  letter-spacing: 1px;
 }
 
 #classinformation {
-  letter-spacing: 2px; 
+  letter-spacing: 2px;
   color: rgba(255, 106, 40);
 }
 #studio {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  color: black; 
+  color: black;
 }
 #credits {
   color: rgba(255, 106, 40);
@@ -122,10 +135,10 @@ export default {
 
 #category {
   border-radius: 20px;
-  background-color:lightgray;
+  background-color: lightgray;
   width: 70px;
-  height: 10px; 
-  color:black;
+  height: 10px;
+  color: black;
   text-transform: uppercase;
   padding: 10px;
   text-align: center;
@@ -133,23 +146,23 @@ export default {
 }
 .map {
   transition: all 0.3s ease-out;
-	display: inline-block;
-	position: relative;
-	overflow: hidden;
-	height: 100%;
-	float: right;
-	width: 50%;
-	display: inline-block;
+  display: inline-block;
+  position: relative;
+  overflow: hidden;
+  height: 100%;
+  float: right;
+  width: 50%;
+  display: inline-block;
 }
 
-.main{
+.main {
   padding-top: 50px;
-  width: 100%; 
+  width: 100%;
   display: flex;
 }
 
 .sub {
-  width: 50%; 
+  width: 50%;
   padding: 30px;
   font-family: Avenir, Helvetica, Arial, sans-serif;
 }
@@ -157,8 +170,8 @@ export default {
 .information {
   background-colour: white;
   box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
-  border-radius: 5px; 
-  padding: 10px; 
+  border-radius: 5px;
+  padding: 10px;
   line-height: 1em;
 }
 
@@ -166,12 +179,12 @@ export default {
   background-color: rgba(255, 106, 40);
   box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
   text-align: center;
-  color: white; 
+  color: white;
   border-radius: 30px;
-  height: 300px; 
+  height: 300px;
   width: 600px;
   display: inline-block;
-	margin: 5px;
+  margin: 5px;
   padding: 20px;
 }
 
@@ -181,8 +194,8 @@ export default {
 }
 
 .calendar {
-	display: inline-block;
-	margin: 5px;
+  display: inline-block;
+  margin: 5px;
 }
 
 .bookingbutton {
@@ -207,12 +220,11 @@ export default {
   border-radius: 5px;
   border: none;
   font-weight: bold;
-  letter-spacing: 1px; 
+  letter-spacing: 1px;
   display: inline-block;
   padding: 1rem 1rem;
   vertical-align: middle;
   margin-right: 15px;
   width: 200px;
 }
-
 </style>
