bd487a53fa859a783840d6196d106e175fcad64e
Po Yan <70246850+ChuanPoYan@users.noreply.github.com>
2022-11-08 15:49:18 +0800

    Added dynamic credits display

--- src/views/ProfileAccount.vue
+++ src/views/ProfileAccount.vue
@@ -1,57 +1,57 @@
 <template>
   <body>
     <div class="bodyalignment">
-    <br><br><br><br><br>
+      <br /><br /><br /><br /><br />
       <div class="main">
-        <div class ="Currentmembershipplan">
+        <div class="Currentmembershipplan">
           <h1>Current membership plan</h1>
-          <div class = "card">
-            <br><br>
+          <div class="card">
+            <br /><br />
             <h2>Intermediate Membership</h2>
-            <hr/>
+            <hr />
             <h3>Purchased Date is 26 October 2022</h3>
             <p>Our package gives you access to 60 credits</p>
-            <p>{{name}}</p>
-            <p>{{address}}</p>
-            <p>{{phone}}</p>
-
+            <p>{{ name }}</p>
+            <p>{{ address }}</p>
+            <p>{{ phone }}</p>
           </div>
         </div>
 
-        <div class ="Credits">
+        <div class="Credits">
           <h1>Credits</h1>
-          <div class = "card2">
-            <h1 style="font-size:60px">14</h1>
-            <hr/>
+          <div class="card2">
+            <h1 style="font-size: 60px">{{ credit }}</h1>
+            <hr />
             <h3>Credits remaining</h3>
             <p>Expiring on 1st October</p>
             <button class="topupbutton">Top Up Credits</button>
           </div>
         </div>
       </div>
-  </div>
-</body>
-<div class="sidenav" style="width:15%">
+    </div>
+  </body>
+  <div class="sidenav" style="width: 15%">
     <router-link to="/profile">Profile</router-link>
     <router-link to="/profile/account">Account</router-link>
     <router-link to="/profile/membership">Membership</router-link>
-    <router-view/>  
+    <router-view />
   </div>
 </template>
 
 <script>
 import firebaseApp from "../main.js";
-import { getFirestore, doc , getDoc} from "firebase/firestore";
+import { getFirestore, doc, getDoc } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const db = getFirestore(firebaseApp);
 
 export default {
-  data(){
-    return{
-      name: this.name,
-      number: this.phone,
-      address: this.address,
+  data() {
+    return {
+      name: null,
+      number: null,
+      address: null,
+      credit: null, 
     };
   },
   created: async function () {
@@ -60,19 +60,18 @@ export default {
     const userRef = doc(db, "users", this.email);
     getDoc(userRef).then((userDoc) => {
       if (userDoc.exists()) {
-        this.name = userDoc.data()["First_Name"]
-        this.phone = userDoc.data()["Phone_Number"]
-        this.address = userDoc.data()["Address"]
+        this.name = userDoc.data()["First_Name"];
+        this.phone = userDoc.data()["Phone_Number"];
+        this.address = userDoc.data()["Address"];
+        this.credit = userDoc.data()["Credits"];
       }
-    })
-  }
-}
-
+    });
+  },
+};
 </script>
 
 <style scoped>
-
-.main{
+.main {
   /* display: grid;
   grid-template-columns:  1fr 1fr; */
   /* justify-content: space-around; */
@@ -84,11 +83,10 @@ export default {
   justify-content: space-evenly;
   width: 85%;
   /* background-color: grey; */
-
 }
 .sidenav {
   margin-top: 95px !important;
-  height: 100%; 
+  height: 100%;
   /* width: 300px; Set the width of the sidebar */
   position: fixed; /* Fixed Sidebar (stay in place on scroll) */
   z-index: 1; /* Stay on top */
@@ -97,8 +95,7 @@ export default {
   background-color: rgba(241, 241, 241, 1); /* Black */
   overflow-x: hidden; /* Disable horizontal scroll */
   padding-top: 40px;
-  font-family: 'Avenir',serif;
-
+  font-family: "Avenir", serif;
 }
 .sidenav a {
   padding: 6px 8px 6px 16px;
@@ -109,13 +106,13 @@ export default {
 }
 .card {
   /* Add shadows to create the "card" effect */
-  box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
   /* transition: 0.3s; */
   border-radius: 5px; /* 5px rounded corners */
   width: 400px;
   height: 300px;
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  float: center; 
+  float: center;
   text-align: center;
 
   /* padding-top: 20px; */
@@ -126,9 +123,9 @@ export default {
   align-content: center;
   justify-content: center;
 } */
-.card2{
+.card2 {
   text-align: center;
-  box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
   border-radius: 5px; /* 5px rounded corners */
   width: 400px;
   height: 300px;
@@ -137,30 +134,30 @@ export default {
 }
 
 /* credits button */
-.topupbutton{ 
-    font-family: 'Avenir', serif;
-    padding: 1rem 3rem;
-    text-align: center;
-    font-size: 16px;
-    text-transform: uppercase;
-    cursor: pointer;
-    background:  rgba(255,106,40);
-    border-radius: 9px;
-    border: none;
-    color: #fff;
-    font-weight: bold;
-    letter-spacing: 1px;
+.topupbutton {
+  font-family: "Avenir", serif;
+  padding: 1rem 3rem;
+  text-align: center;
+  font-size: 16px;
+  text-transform: uppercase;
+  cursor: pointer;
+  background: rgba(255, 106, 40);
+  border-radius: 9px;
+  border: none;
+  color: #fff;
+  font-weight: bold;
+  letter-spacing: 1px;
 }
 .topupbutton:hover {
-  box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
+  box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2);
 }
-hr{
-    display: block;
-    border: none;
-    height: 3px;
-    background-color: rgba(255,106,40);
+hr {
+  display: block;
+  border: none;
+  height: 3px;
+  background-color: rgba(255, 106, 40);
 }
-h1{
+h1 {
   text-align: center;
 }
 </style>
