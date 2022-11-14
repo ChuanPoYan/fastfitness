3eab4d046855efb03bb73e3f595b06f7e9e6335f
TammiChia <e0555851@u.nus.edu>
2022-11-14 15:25:15 +0800

    Remove stuff from BookingListing

--- src/components/BookingListing.vue
+++ src/components/BookingListing.vue
@@ -1,11 +1,9 @@
 <template>
   <article class="article" @click="view()">
     <router-link to="/bookInfo">
-
       <div class="image">
         <img id = "preview" :src="require(`@/${this.classPreview}`)" alt="Booking" style="width:90%"/>
       </div>
-
       <div class="content">
         <h3 class="title">{{ this.Viewing }}</h3>
         <hr class="solid" />
@@ -16,14 +14,6 @@
         <div style="margin-left: 40% ">
           <img src="../assets/run.png" alt="Icon" class="icon" style="height:35px;width:25px;margin-top: 10px;margin-left: 30px;" />
           <span class="post">{{ this.className }}</span>
-        </div >
-        <!-- DATE -->
-        <div>  
-          <h4> Date </h4>
-        </div>
-        <!-- CANCEL BUTTON -->
-        <div>  
-          <button class="cancelbutton"> Cancel Session </button>
         </div>
       </div>
     </router-link>
@@ -148,21 +138,4 @@ export default {
   background-color: grey;
 }
 
-.cancelbutton {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  padding: 20px; 
-  text-align: center;
-  font-size: 16px;
-  text-transform: uppercase;
-  cursor: pointer;
-  background: rgba(255, 106, 40);
-  border-radius: 9px;
-  border: none;
-  color: #fff;
-  font-weight: bold;
-}
-
-.cancelbutton:hover {
-  box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2);
-}
 </style>
